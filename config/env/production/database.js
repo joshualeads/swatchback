6c285767b0ce56101302f16744e75f3e04477dc7
joshualeads <joshuajabakumar@gmail.com>
2024-12-05 19:06:43 +0530

    updating database.js prod file from ts type to js file type

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,22 @@
-import { parse } from "pg-connection-string";
-const config = parse(process.env.DATABASE_URL);
+// path: ./config/env/production/database.js
 
-export default ({ env }) => ({
-  connection: {
-    client: "postgres",
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
       },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+};

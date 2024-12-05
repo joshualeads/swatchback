5ca640e3d9b52d6361d3a2bde953aa38350e18f7
joshualeads <joshuajabakumar@gmail.com>
2024-12-05 17:54:17 +0530

    updating with product creation

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+      v4CompatibilityMode: true,
+    },
+  },
+});

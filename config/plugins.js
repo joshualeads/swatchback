f779fe316e47861955389335cfdc97d114127927
joshualeads <joshuajabakumar@gmail.com>
2024-12-13 15:52:34 +0530

    Updating Strapi to Latest 5.5.1 and enable/disable option for Graphql in Prod

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,24 @@
 module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
   graphql: {
     enabled: true,
     config: {
       endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: false,
+      playgroundAlways: env.GRAPHQL_PLAYGROUND,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {

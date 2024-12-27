d7976d2daa7bb6e0834e454f07c856e16107cd57
joshualeads <joshuajabakumar@gmail.com>
2024-12-27 18:22:42 +0530

    Updating Upload middleware to fix the image thumbnail issue

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,26 @@
+const CLOUDINARY_CDN_URL = "res.cloudinary.com";
+
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", `${CLOUDINARY_CDN_URL}`],
+          "media-src": ["'self'", "data:", "blob:", `${CLOUDINARY_CDN_URL}`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
